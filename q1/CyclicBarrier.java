b284bd14d740dcece5247c538c6980875e2491dc
John Martinez <johnjmartinez@utexas.edu>
2015-06-22 20:11:23 -0500

    More changes. Bug with arrived return. We need to discuss these.

--- q1/CyclicBarrier.java
+++ q1/CyclicBarrier.java
@@ -3,20 +3,23 @@ import java.util.concurrent.Semaphore;
 public class CyclicBarrier {
   // TODO: Declare variables and the constructor for CyclicBarrier
   // Note that you can use only semaphores but not synchronized blocks and
-  // locks 
-	int numParties, arrived;
-	Semaphore mutex;
-	Semaphore[] s;
-	
+  // locks
+    int numParties, arrived;
+    Semaphore mutex;
+    Semaphore[] s;
+
   public CyclicBarrier(int parties) {
     // TODO: The constructor for this CyclicBarrier
-	this.numParties = parties;
-	this.arrived = parties ; //this.arrived = 0;
-	mutex = new Semaphore(1);
-	s = new Semaphore[numParties];
-	for(int i=0; i<numParties; i++){
-		s[i] = new Semaphore(0);
-	}
+    this.numParties = parties;
+    this.arrived = parties ; //this.arrived = 0;
+
+    mutex = new Semaphore(1);
+    s = new Semaphore[numParties];
+
+    for(int i=0; i<numParties; i++) {
+        s[i] = new Semaphore(1); // if init to 0 someone has to release first
+    }
+
   }
 
   public int await() throws InterruptedException {
@@ -24,30 +27,36 @@ public class CyclicBarrier {
     // If the current thread is not the last to arrive then it is
     // disabled for thread scheduling purposes and lies dormant until
     // the last thread arrives.
-    
+
     // Returns: the arrival index of the current thread, where index
     // (parties - 1) indicates the first to arrive and zero indicates
     // the last to arrive.
 
-	//NOT SURE IF NEEDED --- int idx;
-	mutex.acquire(); //Atomically increase partiesArrived
-	//idx = arrived;
-	arrived--; //Per description above //arrived++;
-	
-	if (arrived == 0) { //numParties){
-		//release all semaphores
-		for(int i=1; i<numParties; i++) { //s[0] will never be used
-			s[i].release();
-		//	System.out.println("Barrier Tripped");
-		}
-	} 
-    else {
-		//System.out.println("Idx " + idx + " arrived");
-		s[arrived].acquire(); //Clever ... Make all threads wait // s[arrived-1].acquire();
-	}
+    int idx; //LOCAL, UNIQUE TO EACH THREAD
+    mutex.acquire(); //Atomically decrease arrived
     
-    mutex.release();
-    return arrived;
+    arrived--; //Per description above //arrived++;
+    idx = arrived;
+
+    if (arrived == 0) { //numParties){
+        //Release all semaphores
+        for(int i=1; i<numParties; i++) { //s[0] will never be used
+            s[i].release();
+        }
+        //Reset arrived to numParties again so the barrier can be used again
+        arrived = numParties;
+        //System.out.println("Barrier Tripped; arrived = " + arrived);
+        mutex.release();
+        return idx; //Trying to solve bug below
+    }
+    else {
+        //System.out.println("arrived =  " + arrived);
+        mutex.release(); // has to be released before setting itself in wait mode
+        s[arrived].acquire(); //Clever ... Make threads wait // s[arrived-1].acquire();
+        return idx;
+    }
+   // Bug: Bunch of zeroes and duplicates arriving at the same time  --- return arrived;
+    
   }
-  
+
 }
--- q1/TestCyclicBarrier.java
+++ q1/TestCyclicBarrier.java
@@ -1,76 +1,97 @@
-import java.util.concurrent.*;
+//import java.util.concurrent.CyclicBarrier;
 
 public class TestCyclicBarrier implements Runnable {
 
 	CyclicBarrier cb;
 	int[] a;
 	int[] b;
-	int id,num;
+	int id, num;
 
-	TestCyclicBarrier(CyclicBarrier cb,int[] a,int[] b,int id,int num){
+	TestCyclicBarrier(CyclicBarrier cb, int[] a, int[] b, int id, int num){
 		this.cb = cb;
 		this.a = a;
 		this.b = b;
-		this.id = id;
+		//this.id = id; //Thread id??? CB id??? 
 		this.num = num;
 	}
 
 	public void run() {
-		a[id] = id;
+		//a[id] = id;
+        int a=-1;
 		try{
-			cb.await();
+			a = cb.await(); //returns arrived index
 		}	
-		catch (InterruptedException e) {
+		catch (Exception e) {
             e.printStackTrace();
         }
-
+        
+        System.out.println("\tThread arrived index " + a);
+        
+        
+        /* COMMENTING OUT FOR NOW -- NOT CLEAR WHAT WE'RE TESTING FOR
 		int sum=0;
 		for(int i=0; i<a.length; i++){
 			sum += a[i];
 		}
 		//System.out.println("Id: " + id + " SUM=" + sum);
-		if(id == num - 1){
+		if(id == num - 1) {
 			b[0] = sum;		
-		} else{
+		} 
+        else {
 			b[id+1] = sum;
 		}
+        */
+        
 	}
+    
+    // not working --- public int get_arrived() { return id; }
 
 	public static void main(String[] args) {
-		int N = 10000, sum=0;
+		int N = 100, sum=0;
 		int[] a = new int[N];	
 		int[] b = new int[N];	
+        
 		TestCyclicBarrier[] tcb = new TestCyclicBarrier[N];
 		CyclicBarrier cb = new CyclicBarrier(N);
-		Thread[] t = new Thread[N];
-        
-		for(int i=0; i<N; i++){
-			tcb[i] =new TestCyclicBarrier(cb,a,b,i,N);
-			t[i] = new Thread(tcb[i]);
-			sum += i;
-		}
-
-		for(int i=0; i<N; i++){
-			t[i].start();
-		}
-
-		try {
-			for(int i=0; i<N; i++){
-				t[i].join();
-			}
-		} 
-		catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
 		
-		for(int i=0; i<N; i++){
+        
+		for(int x=0; x<N; x++) {
+            
+			tcb[x] = new TestCyclicBarrier(cb,a,b,x,N);
+            Thread[] t = new Thread[N];
+		
+		    for(int i=0; i<N; i++){
+                // can be reused after all waiting threads have been released
+                t[i] = new Thread(tcb[x]); // t[i] = new Thread(tcb[i]); 
+                sum += i;      
+		    }
+			 
+            for(int i=0; i<N; i++)
+                t[i].start();  
+		    
+            try {
+			    for(int i=0; i<N; i++){
+				    t[i].join(); 
+                    //sum -= t[i].get_arrived(); 
+			    }
+		    } 
+		    catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+             
+            System.out.println ("Iteration "+x+"\tsum is "+sum);
+            sum=0; 
+		}
+        
+		/* SEE ABOVE
+         * for(int i=0; i<N; i++){
 			if(a[i] != i){
 				System.out.println("ERROR: a[" + i + "] didn't match expected value. Observed:" + a[i] + " Expected: " + i); 
 			}
 			if(b[i] != sum){
 				System.out.println("ERROR: b[" + i + "] didn't match expected value. Observed:" + b[i] + " Expected: " + sum); 
 			}
-		}
+		}*/
+        
 	}
 }
